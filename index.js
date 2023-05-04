492a1c1135ca62cf7d1454778add1eaf00287799
lain-alice <freya6829@gmail.com>
2023-05-04 23:14:51 +0900

    feat: JS practice

---
+++ index.js
@@ -0,0 +1,43 @@
+// object shorthand assignment]
+
+// let name = "babo";
+// let age = 17;
+
+// let person = {
+//   name,
+//   age,
+// };
+
+// console.log(person);
+
+// Destructuring 구조분해할당
+
+// let person = {
+//   name: "babo",
+//   age: 20,
+// };
+
+// let name = person.name;
+// let age = person["age"];
+
+// let { name, age } = person;
+
+// console.log(name, age);
+
+// let array = [1, 2, 3, 4];
+// let [a, b, ...rest] = array;
+
+// console.log(rest);
+
+// spread
+
+let person = { name: "noona", age: 12 };
+let person2 = { ...person, name: "babo", address: "의정부" };
+console.log(person2);
+
+console.log(person === person2);
+
+let a = [1, 2];
+let b = [...a, 3];
+let c = [...a, ...b];
+console.log(c);
